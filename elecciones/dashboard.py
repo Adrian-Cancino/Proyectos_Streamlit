db4791de53c2fda9b10b4b63513720e8cb11a4ff
Adrian Cancino <71229190+Adrian-Cancino@users.noreply.github.com>
2024-12-16 16:45:58 -0600

    Agregamos graficos por distrito

--- elecciones/dashboard.py
+++ elecciones/dashboard.py
@@ -9,43 +9,41 @@ casillas_path = os.path.join(BASE_DIR, 'data', 'casillas_limpio.csv')
 elecciones_gob_2021_path = os.path.join(BASE_DIR, 'data', 'votos_gob_2021.csv')
 
 st.title('Resultados gobernador 2021')
-st.write('Este dashboard tiene información sobre las casillas del INE durante'
-         'las elecciones del 2021')
+st.write('Este dashboard está elaborado con datos del INE y el CEEPAC')
+st.divider()
 
 data_casillas_2021 = pd.read_csv(casillas_path)
 votos_gob = pd.read_csv(elecciones_gob_2021_path)
 data_casillas_2021[['Sección', 'Padrón Electoral', 'Listado Nominal', 'postal_code']] = data_casillas_2021[['Sección', 'Padrón Electoral', 'Listado Nominal', 'postal_code']].astype('object')
 
-# FILTRADO POR DISTRITO FEDERAL
-col1, col2, col3 = st.columns(3)
-distritos_federales = ['Todos'] + list(data_casillas_2021['Distrito Federal'].unique())
-with col1:
+with st.sidebar:
+
+    # FILTRADO POR DISTRITO FEDERAL
+    distritos_federales = ['Todos'] + list(data_casillas_2021['Distrito Federal'].unique())
     distrito_federal_casilla = st.selectbox('Distrito Federal', distritos_federales)
 
-if distrito_federal_casilla == 'Todos':
-    data_filtrado_federal = data_casillas_2021
-else:
-    data_filtrado_federal = data_casillas_2021[data_casillas_2021['Distrito Federal'] == distrito_federal_casilla]
+    if distrito_federal_casilla == 'Todos':
+        data_filtrado_federal = data_casillas_2021
+    else:
+        data_filtrado_federal = data_casillas_2021[data_casillas_2021['Distrito Federal'] == distrito_federal_casilla]
 
-#FILTRADO POR DISTRITO LOCAL
-distritos_locales = ['Todos'] + list(data_filtrado_federal['Distrito Local'].unique())
-with col2:
+    #FILTRADO POR DISTRITO LOCAL
+    distritos_locales = ['Todos'] + list(data_filtrado_federal['Distrito Local'].unique())
     distrito_local_casilla = st.selectbox('Distrito Local', distritos_locales)
 
-if distrito_local_casilla == 'Todos':
-    data_filtrado_local = data_filtrado_federal
-else:
-    data_filtrado_local = data_filtrado_federal[data_filtrado_federal['Distrito Local'] == distrito_local_casilla]
+    if distrito_local_casilla == 'Todos':
+        data_filtrado_local = data_filtrado_federal
+    else:
+        data_filtrado_local = data_filtrado_federal[data_filtrado_federal['Distrito Local'] == distrito_local_casilla]
 
-# FILTRADO MUNICIPIOS
-municipios = ['Todos'] + list(data_filtrado_local['Municipio'].unique())
-with col3:
+    # FILTRADO MUNICIPIOS
+    municipios = ['Todos'] + list(data_filtrado_local['Municipio'].unique())
     municipio_casilla = st.selectbox('Municipio', municipios)
 
-if municipio_casilla == 'Todos':
-    data_filtrado_municipio = data_filtrado_local
-else:
-    data_filtrado_municipio = data_filtrado_local[data_filtrado_local['Municipio'] == municipio_casilla]
+    if municipio_casilla == 'Todos':
+        data_filtrado_municipio = data_filtrado_local
+    else:
+        data_filtrado_municipio = data_filtrado_local[data_filtrado_local['Municipio'] == municipio_casilla]
 
 #st.write(data_filtrado_municipio.head())
 total_padron_nominal = data_filtrado_municipio['Padrón Electoral'].astype(int).sum()
@@ -72,15 +70,44 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.metric(label="Votos Morena", value=votos_morena)
 with col2:
-    st.metric(label="Porcentaje", value=f"{porcentaje}%")
+    st.metric(label="Porcentaje", value=porcentaje)
 with col3:
     st.metric(label="Votos Totales", value=todos_votos)
 
 fig = px.bar(
     x=suma_partidos.index,
     y=suma_partidos.values,
-    labels={'x': 'Partido', 'y': 'Suma'},
-    title='Suma de valores por partido'
+    labels={'x': 'Partido', 'y': 'Votos'},
+    title='Total de votos por partido partido'
 )
 
-st.plotly_chart(fig)
+suma_votos = votos_gob.groupby('DISTRITO')[['PAN',
+        'PRI', 'PRD', 'PT', 'PVEM', 'PCP', 'PMC', 'MORENA', 'PNA SAN LUIS',
+        'PES', 'RSP', 'FXM', 'CI']].sum()
+suma_votos['Total'] = suma_votos.sum(axis=1)
+
+fig_distritos = px.bar(
+    x=suma_votos.index,
+    y=suma_votos['MORENA'].values,
+    labels={'x': 'Distritos', 'y': 'Votos'},
+    title='Total de votos para MORENA por distrito'
+)
+municipios = list(suma_votos.index)
+
+tab1, tab2 = st.tabs(["Partidos", "Distritos"])
+
+with tab1:
+    st.plotly_chart(fig)
+with tab2:
+    seleccion_distrito = st.selectbox('Distrito', municipios)
+    col1, col2, col3 = st.columns(3)
+    votos_distrito = suma_votos.loc[[seleccion_distrito]]
+    porcentaje_morena = round((votos_distrito['MORENA'] * 100) / votos_distrito['Total'], 2)
+    with col1:
+        st.metric(label="Votos Morena", value=votos_distrito['MORENA'])
+    with col2:
+        st.metric(label="Porcentaje", value= porcentaje_morena)
+    with col3:
+        st.metric(label="Votos Totales", value=votos_distrito['Total'])
+    #st.write(votos_distrito.head())
+    st.plotly_chart(fig_distritos)
